0c8392521fef01c92f322f3432010d1046db7d39
tiago <tiago@toucantech.com>
2021-11-27 12:51:57 +0000

    Delete utils and and analyser

---
+++ src/Analysis/Analyser.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Xbugszone\Cryptotools\Analysis;
+
+class Analyser
+{
+    /**
+     * The ticker to analyse
+     * @var array Ticker
+     */
+    protected array $ticker;
+
+    /**
+     * The tickers to analyse
+     * @var array Tickers
+     */
+    protected array $tickers;
+
+    /**
+     * Set Ticker
+     * @param array $ticker
+     */
+    public function setTicker(array $ticker): void {
+        $this->ticker = $ticker[0];
+    }
+
+    /**
+     * Set Tickers
+     * @param array $tickers
+     */
+    public function setTickers(array $tickers): void {
+        $this->tickers = $tickers;
+    }
+
+    /**
+     * Get Average
+     * @return float Average
+     */
+    public function getAvg() : float {
+        return ($this->ticker['high']+$this->ticker['low'])/2;
+    }
+
+    /**
+     * Get the diference
+     * @return float Difference
+     */
+    public function getDiff() : float {
+        return $this->ticker['open'] - $this->ticker['close'];
+    }
+
+    /**
+     * Get the change
+     * @param array $ticker
+     * @return float %
+     */
+    public function getChange(array $ticker) : float {
+        return $this->getDiff()/($this->getAvg()/100);
+    }
+
+    /**
+     * Get the direction
+     * -1 down
+     * 0 no direction
+     * 1 up
+     * @return int
+     */
+    public function direction() : int {
+        return 0;
+    }
+}
